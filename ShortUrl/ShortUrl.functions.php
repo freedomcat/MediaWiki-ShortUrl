dcd399e68f71c57f98f8058fc0a66c9892edf5d5
Yuvaraj Pandian T <yuvipanda@gmail.com>
2011-04-20 00:17:53 +0530

    Moved from base36 to base64

--- ShortUrl/ShortUrl.functions.php
+++ ShortUrl/ShortUrl.functions.php
@@ -13,10 +13,27 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	exit( 1 );
 }
 
+/* stolen from http://www.php.net/manual/en/function.base64-encode.php#63543 */
+function urlsafe_b64encode($string) {
+	    $data = base64_encode($string);
+		    $data = str_replace(array('+','/','='),array('-','_',''),$data);
+		    return $data;
+}
+
+/* stolen from http://www.php.net/manual/en/function.base64-encode.php#63543 */
+function urlsafe_b64decode($string) {
+	    $data = str_replace(array('-','_'),array('+','/'),$string);
+		    $mod4 = strlen($data) % 4;
+		    if ($mod4) {
+				        $data .= substr('====', $mod4);
+						    }
+			    return base64_decode($data);
+}
+
 function shorturlEncode ( $id ) {
-	return base_convert ( $id, 10, 36 );
+	return urlsafe_b64encode( $id );
 }
 
 function shorturlDecode ( $data ) {
-	return base_convert ( $data, 36, 10 );
+	return intval(urlsafe_b64decode ( $data ));
 }
